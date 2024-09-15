5e0b0b4dcb0459639df2b5b14673c7e064b72388
ZhangJian He <shoothzj@gmail.com>
2024-09-15 15:15:17 +0800

    chore: clean up useless method (#7)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- zk/client.go
+++ zk/client.go
@@ -2,17 +2,14 @@ package zk
 
 import (
 	"crypto/tls"
-	"fmt"
-	"github.com/shoothzj/gox/buffer"
 	"github.com/shoothzj/gox/netx"
-	"net"
-	"sync"
+	"math/rand"
 	"sync/atomic"
 	"time"
 )
 
 type Config struct {
-	Address          netx.Address
+	Addresses        []netx.Address
 	SendQueueSize    int
 	PendingQueueSize int
 	BufferMax        int
@@ -21,13 +18,8 @@ type Config struct {
 }
 
 type Client struct {
-	config       *Config
-	client       *ProtocolClient
-	conn         net.Conn
-	eventsChan   chan *sendRequest
-	pendingQueue chan *sendRequest
-	buffer       *buffer.Buffer
-	closeCh      chan struct{}
+	config *Config
+	client *ProtocolClient
 
 	transactionId atomic.Int32
 }
@@ -102,105 +94,6 @@ func (c *Client) nextTransactionId() int {
 	return int(c.transactionId.Add(1))
 }
 
-func (c *Client) Send(bytes []byte) ([]byte, error) {
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	var result []byte
-	var err error
-	c.sendAsync(bytes, func(resp []byte, e error) {
-		result = resp
-		err = e
-		wg.Done()
-	})
-	wg.Wait()
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (c *Client) sendAsync(bytes []byte, callback func([]byte, error)) {
-	sr := &sendRequest{
-		bytes:    bytes,
-		callback: callback,
-	}
-	c.eventsChan <- sr
-}
-
-func (c *Client) read() {
-	for {
-		select {
-		case req := <-c.pendingQueue:
-			n, err := c.conn.Read(c.buffer.WritableSlice())
-			if err != nil {
-				req.callback(nil, err)
-				c.closeCh <- struct{}{}
-				break
-			}
-			err = c.buffer.AdjustWriteCursor(n)
-			if err != nil {
-				req.callback(nil, err)
-				c.closeCh <- struct{}{}
-				break
-			}
-			if c.buffer.Size() < 4 {
-				continue
-			}
-			bytes := make([]byte, 4)
-			err = c.buffer.ReadExactly(bytes)
-			c.buffer.Compact()
-			if err != nil {
-				req.callback(nil, err)
-				c.closeCh <- struct{}{}
-				break
-			}
-			length := int(bytes[3]) | int(bytes[2])<<8 | int(bytes[1])<<16 | int(bytes[0])<<24
-			if c.buffer.Size() < length {
-				continue
-			}
-			// in case ddos attack
-			if length > c.buffer.Capacity() {
-				req.callback(nil, fmt.Errorf("response length %d is too large", length))
-				c.closeCh <- struct{}{}
-				break
-			}
-			data := make([]byte, length)
-			err = c.buffer.ReadExactly(data)
-			if err != nil {
-				req.callback(nil, err)
-				c.closeCh <- struct{}{}
-				break
-			}
-			c.buffer.Compact()
-			req.callback(data, nil)
-		case <-c.closeCh:
-			return
-		}
-	}
-}
-
-func (c *Client) write() {
-	for {
-		select {
-		case req := <-c.eventsChan:
-			n, err := c.conn.Write(req.bytes)
-			if err != nil {
-				req.callback(nil, err)
-				c.closeCh <- struct{}{}
-				break
-			}
-			if n != len(req.bytes) {
-				req.callback(nil, fmt.Errorf("write %d bytes, but expect %d bytes", n, len(req.bytes)))
-				c.closeCh <- struct{}{}
-				break
-			}
-			c.pendingQueue <- req
-		case <-c.closeCh:
-			return
-		}
-	}
-}
-
 func (c *Client) Close() {
 	c.client.Close()
 }
@@ -216,7 +109,9 @@ func NewClient(config *Config) (*Client, error) {
 		config.BufferMax = 512 * 1024
 	}
 
-	protocolClient, err := NewProtocolClient(config.Address, config)
+	selectedAddress := config.Addresses[rand.Intn(len(config.Addresses))]
+
+	protocolClient, err := NewProtocolClient(selectedAddress, config)
 	if err != nil {
 		return nil, err
 	}
