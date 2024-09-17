7235ac906ee56176d5cbecc6bafca7c9bd0d1114
ZhangJian He <shoothzj@gmail.com>
2024-09-17 10:05:31 +0800

    fix: protocol client deadlock when send request after close
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- zk/protocol_client_test.go
+++ zk/protocol_client_test.go
@@ -30,3 +30,29 @@ func TestProtocolClientConnect(t *testing.T) {
 	require.Nil(t, err)
 	assert.NotNil(t, resp)
 }
+
+func TestProtocolClientConnectAfterClose(t *testing.T) {
+	client, err := NewProtocolClient(netx.Address{
+		Host: "localhost",
+		Port: 2181,
+	}, &Config{
+		SendQueueSize:    100,
+		PendingQueueSize: 100,
+		BufferMax:        1024,
+	})
+	require.NoError(t, err)
+	req := &ConnectReq{
+		ProtocolVersion: 0,
+		LastZxidSeen:    0,
+		Timeout:         30_000,
+		SessionId:       0,
+		Password:        PasswordEmpty,
+		ReadOnly:        false,
+	}
+	resp, err := client.Connect(req)
+	require.Nil(t, err)
+	assert.NotNil(t, resp)
+	client.Close()
+	_, err = client.Connect(req)
+	assert.Equal(t, ErrClientClosed, err)
+}
